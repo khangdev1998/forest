cc58942511ade31a1aa116d382c7955f03fa1c5b
khang-kunz <khangdev171998@gmail.com>
2023-08-09 21:39:00 +0700

    update animate

--- js/home.js
+++ js/home.js
@@ -34,22 +34,124 @@ $(document).ready(function () {
   observer.observe(video);
 
   // Scroll Page Effect Section
-  let hasAnimatedNews = false;
   let hasAnimatedHeader = false;
   let scrollPastHeader = false;
+  let animatedSections = {};
 
   $("#fullpage").fullpage({
     onLeave: function (origin, destination, direction) {
-      const sectionId = destination.item.id;
-      if (sectionId === "section-news" && !hasAnimatedNews) {
-        hasAnimatedNews = true;
-        gsap.from(".news__content-pane.active .news__content-item", 1, {
-          duration: 2,
-          y: 150,
-          opacity: 0,
-          ease: "power1.out",
-          stagger: 0.6,
+      const sectionId = destination.anchor;
+      function headingAnimated(section) {
+        const heading = $(`${section} .heading`);
+        gsap.set(heading, { opacity: 0, rotationY: -90 });
+        gsap.to(heading, {
+          opacity: 1,
+          rotationY: 0,
+          duration: 1.6,
+          delay: 0.5, // thêm delay 0.5 giây
+          ease: "power2.out",
         });
+      }
+      function descriptionAnimated(element) {
+        gsap.from(`${element}`, {
+          opacity: 0,
+          y: 100, // Di chuyển phần tử xuống 100px so với vị trí ban đầu
+          duration: 1.4,
+          ease: "power2.out",
+        });
+      }
+      function slideInFrom(element, direction) {
+        let delay = 0;
+        // Lặp qua mỗi mục và áp dụng hiệu ứng animation
+        $(`${element}`).each(function () {
+          gsap.from($(this), {
+            x: direction, // Di chuyển mục từ trái sang 100px so với vị trí ban đầu
+            opacity: 0,
+            duration: 1.2,
+            ease: "power2.out",
+            delay: delay,
+          });
+          delay += 0.3; // Tăng thời gian trễ cho mỗi mục tiếp theo
+        });
+      }
+      if (!animatedSections[sectionId]) {
+        if (sectionId === "section2") {
+          headingAnimated(".forest");
+          descriptionAnimated(".forest__description");
+        }
+        if (sectionId === "section3") {
+          headingAnimated(".exhibition");
+          descriptionAnimated(".exhibition__item-desc");
+        }
+        if (sectionId === "section4") {
+          descriptionAnimated(".news__tab");
+          headingAnimated(".news");
+
+          let newsItems = $(".news__content-item");
+          newsItems.each(function (index, item) {
+            // Thiết lập ban đầu cho mỗi item: ẩn nó và dịch chuyển xuống một khoảng cách nhất định
+            gsap.set(item, {
+              opacity: 0,
+              y: 50,
+            });
+            // Tạo một hiệu ứng xuất hiện mượt mà cho mỗi item khi nó xuất hiện trong viewport
+            gsap.to(item, {
+              opacity: 1,
+              y: 0,
+              delay: index * 0.3, // Thêm một khoảng trễ giữa mỗi item để tạo hiệu ứng tuần tự
+              duration: 1.5, // Thời gian diễn ra của hiệu ứng
+              ease: "power3.out", // Loại easing cho hiệu ứng mượt mà
+            });
+          });
+        }
+        if (sectionId === "section5") {
+          headingAnimated(".support");
+          slideInFrom(".support__column", 100);
+        }
+        if (sectionId === "section6") {
+          headingAnimated(".library");
+          descriptionAnimated(".library__desc");
+          slideInFrom(".library__wrapper-item", -100);
+        }
+        if (sectionId === "section7") {
+          $(".footer__copyright-line").css(
+            "animation",
+            "expandLine 4s forwards"
+          );
+          gsap.from(".footer__wrapper-item", {
+            opacity: 0,
+            y: 50, // Move items up by 50 pixels initially
+            stagger: 0.3, // Delay of 0.3 seconds between each item
+            duration: 1, // Duration of the animation
+            ease: "Power4.out", // Ease type
+          });
+
+          // Lấy tất cả các footer slider items
+          let sliderItems = $(".footer__slider-item");
+
+          // Thiết lập ban đầu cho mỗi item: ẩn nó và thiết lập góc xoay cho hiệu ứng lật
+          gsap.set(sliderItems, {
+            opacity: 0,
+            rotationX: -90,
+          });
+
+          // Tạo hiệu ứng lật 3D từ dưới lên trên cho mỗi item khi nó xuất hiện trong viewport
+          let animations = []; // mảng chứa tất cả animations
+          sliderItems.each(function (index, item) {
+            animations.push(
+              gsap.to(item, {
+                opacity: 1,
+                rotationX: 0,
+                transformOrigin: "center bottom",
+                delay: index * 0.1, // Thêm một khoảng trễ giữa mỗi item để tạo hiệu ứng tuần tự
+                duration: 1.5, // Thời gian diễn ra của hiệu ứng
+                ease: "power3.out", // Loại easing cho hiệu ứng mượt mà
+              })
+            );
+          });
+        }
+
+        animatedSections[sectionId] = true; // Đánh dấu section đã được animate
       }
     },
     afterRender: function () {
